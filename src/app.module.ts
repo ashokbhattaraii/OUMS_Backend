05920ef18bb8499630523498b7101b29faf1bc6c
Ashok <ashok.bhattarai@rumsan.net>
2026-02-23 12:01:13 +0545

    feat: request can be created successfully

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,10 @@ import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { SupabaseService } from './supabase/supabase.service';
 import { SupabaseModule } from './supabase/supabase.module';
+import { RequestModule } from './request/request.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule, AuthModule, SupabaseModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule, AuthModule, SupabaseModule, RequestModule],
   controllers: [AppController],
   providers: [AppService, SupabaseService],
 })
